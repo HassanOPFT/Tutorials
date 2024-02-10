e37daeb9f8a1ff96129e4463891564e4594f2694
Hassan Mustafa <43503844+HassanOPFT@users.noreply.github.com>
2024-02-10 13:05:58 +0800

    Create sumCalculator.py

---
+++ sumCalculator.py
@@ -0,0 +1,13 @@
+first = 0
+second = 0
+sum = 0
+
+first_input = input("Enter the first number: ")
+first = int(first_input)
+
+second_input = input("Enter the second number: ")
+second = int(second_input)
+
+sum = first + second
+
+print("The sum of", first, "and", second, "is:", sum)
